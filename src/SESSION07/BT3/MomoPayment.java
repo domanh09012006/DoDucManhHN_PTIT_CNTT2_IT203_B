6fd4f326b63265cb427930e05a360f288cbfe6b5
domanh09012006 <doducmanh09012006@gmail.com>
2026-03-16 23:20:13 +0700

    Session07

--- src/SESSION07/BT3/MomoPayment.java
+++ src/SESSION07/BT3/MomoPayment.java
@@ -5,4 +5,8 @@ public class MomoPayment implements EWalletPayable {
     public void processEWallet(double amount) {
         System.out.println("Xử lý thanh toán MoMo: " + amount + " - Thành công");
     }
+    @Override
+    public void processPayment(double amount) {
+
+    }
 }
